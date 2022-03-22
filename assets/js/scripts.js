62d56a22153d8694ec28a34a7faa8998f39ce506
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-03-22 17:18:57 -0600

    Busqueda al precionar enter

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,40 +2,45 @@ const pokeNameInfo = document.getElementById('pokeName-view');
 const statInfo = document.getElementById('pantalla-info');
 const typeScreen = document.getElementById('type-screen');
 const idScreen = document.getElementById('id-screen');
+const input = document.getElementById('pokeName');
+
+input.addEventListener("keyup", function(event){
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        document.getElementById('buscarPokemon').click();
+    }
+});
 
 
 const fetchPokemon = () => {
-    const pokeName = document.getElementById('pokeName').value.toLowerCase();
+    const pokeName = input.value.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
 
     fetch(url).then((res) => {
         if(res.status != "200"){
-            console.log("Resultado", res)
             pokeImage("./assets/img/404.gif")
             pokeNameInfo.innerHTML = "";
             statInfo.innerHTML = "";
             statInfo.innerText = "No información"
             idScreen.innerHTML = '#-';
             typeScreen.innerText = '-';
+            input.value = "";
         }else{
             return res.json();
         }
     }).then((data) => {
-        console.log('Data: ', data);
         let pokeImg = data.sprites.front_default;
         let namePokemon = data.species.name;
         let stats = data.stats;
         let type = data.types[0].type.name;
         let id = data.id;
 
+        input.value = "";
 
-        console.log(pokeImg, namePokemon, stats, type)
         pokeDatos(namePokemon, stats, type, id) 
         pokeImage(pokeImg)
     });
 }
-
-//fetchPokemon();
 
 const pokeDatos = (name, stats, type, id) => {
     
@@ -55,5 +60,3 @@ const pokeImage = (url) =>{
     const pokeImg = document.getElementById('pokeImg-insert');
     pokeImg.src = url;
 }
-
-//pokeImage('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/152.png')
